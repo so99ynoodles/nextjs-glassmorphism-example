801ecd784eb9cca3da5e43837c6bf14462a32a17
Taiki Ikeda <so99ynoodles@TaikinoMacBook-Pro.local>
2021-01-02 01:55:13 +0900

    Add ambientLight for texture color

--- src/components/Model.tsx
+++ src/components/Model.tsx
@@ -1,12 +1,22 @@
 import React, { Suspense, useEffect, useState } from 'react'
 import { Canvas } from 'react-three-fiber'
+import { Color } from 'three';
 
 const LoadModel = () => {
 	const [model, setModel] = useState<any>();
 	useEffect(() => {
 		const loadModel = async () => {
 			const GLTFLoader = (await import('three/examples/jsm/loaders/GLTFLoader')).GLTFLoader;
-			new GLTFLoader().load("./pony_cartoon/scene.gltf", gltf => setModel(gltf), undefined, error => console.error(error));
+			new GLTFLoader().load("./pony_cartoon/scene.gltf", (gltf: any) => {
+				setModel(gltf)
+				// console.log(gltf)
+				const meshes = gltf?.scene.children[0].children[0].children[0].children;
+				meshes.forEach(mesh => {
+					console.log(mesh)
+					mesh.material.color = new Color(1, 1, 1)
+				})
+				console.log()
+			}, undefined, error => console.error(error));
 		}
 		loadModel();
 	}, [])
@@ -26,6 +36,7 @@ const UseModel = () => {
 const Model = () => {
 	return (
 		<Canvas>
+			<ambientLight color="0xffffff" />
 			<mesh>
 				<UseModel />
 			</mesh>
