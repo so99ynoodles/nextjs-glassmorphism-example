a1a70268a44ad704eaf55c828f416a694dc598b0
Taiki Ikeda <so99ynoodles@TaikinoMacBook-Pro.local>
2021-01-09 22:10:25 +0900

    Optimization

--- next.config.js
+++ next.config.js
@@ -20,10 +20,6 @@ module.exports = {
       },
     ];
   },
-  i18n: {
-    locales: ['en', 'ja', 'kr'],
-    defaultLocale: 'ja',
-  },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.plugins.push(new RemoveServiceWorkerPlugin());
     return config;
