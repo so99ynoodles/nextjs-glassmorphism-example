55f573e02d20dcd130b0a686d12a6b6e820d6783
Taiki Ikeda <so99ynoodles@TaikinoMacBook-Pro.local>
2021-01-10 07:45:15 +0900

    Fix

--- src/pages/blog/tags/index.tsx
+++ src/pages/blog/tags/index.tsx
@@ -1,14 +1,6 @@
 import React from 'react';
 import { GetStaticProps } from 'next';
-import { Heading, Divider } from '@chakra-ui/react';
 import { FaHome, FaPencilAlt } from 'react-icons/fa';
-
-import { ArticleListLayout } from '../../../components/templates/ArticleListLayout';
-import { HtmlHead } from '../../../components/atoms/HtmlHead';
-import { BackLinks } from '../../../components/molecules/BackLinks';
-import { TagListWithCount } from '../../../components/molecules/TagList/WithCount';
-
-import { urlContentsBlog, urlBlogRoot, urlBlogTags } from '../../url.json';
 import { getArticles } from '../../../utils/article/fs.server';
 import {
   comparatorTagCount,
@@ -23,40 +15,24 @@ type IndexPageProps = {
   orderByCount: TagAndCount[];
 };
 
-export const IndexPage: React.FC<IndexPageProps> = ({ orderByName, orderByCount }) => (
-  <ArticleListLayout title="Tags">
-    <HtmlHead title="Tags" url={urlBlogTags} />
-
-    <Heading as="h2" fontSize="2xl" mb={4}>
-      by Name
-    </Heading>
-
-    <TagListWithCount tagAndCounts={orderByName} tagBaseUrl={urlBlogTags} />
-
-    <Heading as="h2" fontSize="2xl" mt={12} mb={4}>
-      by Count
-    </Heading>
-
-    <TagListWithCount tagAndCounts={orderByCount} tagBaseUrl={urlBlogTags} />
-
-    <Divider mt={12} mb={8} />
-
-    <BackLinks
-      links={[
-        { to: urlBlogRoot, icon: FaPencilAlt, label: 'Back to Blog Index' },
-        { to: '/', icon: FaHome, label: 'Back to Home' },
-      ]}
-    />
-  </ArticleListLayout>
-);
+export const IndexPage: React.FC<IndexPageProps> = ({
+  orderByName,
+  orderByCount,
+}) => <></>;
 
 export default IndexPage;
 
 export const getStaticProps: GetStaticProps = async () => {
-  const articles = await getArticles(urlContentsBlog);
+  const articles = await getArticles('/posts');
   const tagAndCountTable = getTableWithTagAndCountIncludedInArticles(articles);
-  const orderByName = getArrayOfTagAndCountFromTable(tagAndCountTable, comparatorTagName);
-  const orderByCount = getArrayOfTagAndCountFromTable(tagAndCountTable, comparatorTagCount);
+  const orderByName = getArrayOfTagAndCountFromTable(
+    tagAndCountTable,
+    comparatorTagName
+  );
+  const orderByCount = getArrayOfTagAndCountFromTable(
+    tagAndCountTable,
+    comparatorTagCount
+  );
 
   return { props: { orderByName, orderByCount } as IndexPageProps };
 };
