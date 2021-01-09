8e1c39e935d341c7b33e1d329bb127bd4a30366e
Taiki Ikeda <so99ynoodles@TaikinoMacBook-Pro.local>
2021-01-09 15:48:06 +0900

    Remove picture

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import styled from '@emotion/styled';
-import Image from 'next/image';
 import Head from 'next/head';
 import { GithubIcon } from '../assets/icons/Github';
 import { TwitterIcon } from '../assets/icons/Twitter';
@@ -31,24 +30,6 @@ const Section = styled.section`
 
   @media ${mq.max.mobile} {
     padding: 1rem 0.5rem;
-  }
-`;
-
-const Avatar = styled(motion.div)`
-  border-radius: 50%;
-  overflow: hidden;
-  background: var(--primary-color);
-  border: 3px solid var(--border-color);
-  max-width: 240px;
-  max-height: 240px;
-  box-shadow: var(--box-shadow-lg);
-  margin-bottom: 2rem;
-
-  @media ${mq.max.laptop} {
-    max-width: 64px;
-    max-height: 64px;
-    box-shadow: var(--box-shadow-md);
-    margin-right: 1.5rem;
   }
 `;
 
@@ -96,106 +77,46 @@ export default function Home({ allPostsData }) {
       </Head>
       <Container>
         <Section>
-          <Responsive mobile tablet laptop>
-            <UserHeading>
-              <Avatar>
-                <Image
-                  alt="Avatar"
-                  width={64}
-                  height={64}
-                  src="/images/goat.png"
-                />
-              </Avatar>
-              <div>
-                <SubHeading>Hi, I'm a</SubHeading>
-                <Heading>Developer.</Heading>
-                <Links>
-                  <li>
-                    <a
-                      rel="noopener noreferrer"
-                      target="_blank"
-                      href="https://github.com/so99ynoodles"
-                    >
-                      <GithubIcon fill="#000" size="2rem" />
-                      <VisuallyHidden elementType="span">
-                        Github Page
-                      </VisuallyHidden>
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      rel="noopener noreferrer"
-                      target="_blank"
-                      href="https://twitter.com/so99ynoodles"
-                    >
-                      <TwitterIcon fill="#1da1f2" size="2rem" />
-                      <VisuallyHidden elementType="span">
-                        Twitter Page
-                      </VisuallyHidden>
-                    </a>
-                  </li>
-                </Links>
-              </div>
-            </UserHeading>
-          </Responsive>
-          <Responsive desktop>
-            <Avatar
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ duration: 0.5 }}
-            >
-              <Image
-                alt="Avatar"
-                width={240}
-                height={240}
-                src="/images/goat.png"
-              />
-            </Avatar>
-            <SubHeading
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ delay: 0.5, duration: 0.5 }}
-            >
-              Hi, I'm a
-            </SubHeading>
-            <Heading
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ delay: 1, duration: 0.5 }}
-            >
-              Developer.
-            </Heading>
-            <Links
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ delay: 1.5, duration: 0.5 }}
-            >
-              <li>
-                <a
-                  rel="noopener noreferrer"
-                  target="_blank"
-                  href="https://github.com/so99ynoodles"
-                >
-                  <GithubIcon fill="#000" size="2rem" />
-                  <VisuallyHidden elementType="span">
-                    Github Page
-                  </VisuallyHidden>
-                </a>
-              </li>
-              <li>
-                <a
-                  rel="noopener noreferrer"
-                  target="_blank"
-                  href="https://twitter.com/so99ynoodles"
-                >
-                  <TwitterIcon fill="#1da1f2" size="2rem" />
-                  <VisuallyHidden elementType="span">
-                    Twitter Page
-                  </VisuallyHidden>
-                </a>
-              </li>
-            </Links>
-          </Responsive>
+          <SubHeading
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.5, duration: 0.5 }}
+          >
+            Hi, I'm a
+          </SubHeading>
+          <Heading
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1, duration: 0.5 }}
+          >
+            Developer.
+          </Heading>
+          <Links
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1.5, duration: 0.5 }}
+          >
+            <li>
+              <a
+                rel="noopener noreferrer"
+                target="_blank"
+                href="https://github.com/so99ynoodles"
+              >
+                <GithubIcon fill="#000" size="2rem" />
+                <VisuallyHidden elementType="span">Github Page</VisuallyHidden>
+              </a>
+            </li>
+            <li>
+              <a
+                rel="noopener noreferrer"
+                target="_blank"
+                href="https://twitter.com/so99ynoodles"
+              >
+                <TwitterIcon fill="#1da1f2" size="2rem" />
+                <VisuallyHidden elementType="span">Twitter Page</VisuallyHidden>
+              </a>
+            </li>
+          </Links>
         </Section>
         <Section>
           <Responsive desktop>
