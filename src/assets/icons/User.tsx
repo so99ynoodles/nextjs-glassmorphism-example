7055ad9b2df095ebd13afbb9918f0971a229768c
Taiki Ikeda <so99ynoodles@TaikinoMacBook-Pro.local>
2021-01-10 18:30:40 +0900

    Fix

--- src/assets/icons/User.tsx
+++ src/assets/icons/User.tsx
@@ -1,26 +1,19 @@
-import { motion, MotionProps } from 'framer-motion';
 import { cx } from '../../../lib/classnames';
 
-export const UserIcon: React.FC<TwoColorIconProps & MotionProps> = ({
+export const UserIcon: React.FC<TwoColorIconProps> = ({
   fill,
   mainFill,
   subFill,
   size = '1rem',
   isActive,
   className,
-  ...props
 }) => (
-  <motion.svg
+  <svg
     className={className}
     width={size}
     height={size}
     viewBox="0 0 48 48"
     xmlns="http://www.w3.org/2000/svg"
-    whileHover={{
-      scale: 1.2,
-    }}
-    whileTap={{ scale: 0.9 }}
-    {...props}
   >
     <path
       className={cx('path-sub', {
@@ -36,5 +29,5 @@ export const UserIcon: React.FC<TwoColorIconProps & MotionProps> = ({
       d="M42 40V38C42 35.3478 40.9464 32.8043 39.0711 30.9289C37.1957 29.0536 34.6522 28 32 28H16C13.3478 28 10.8043 29.0536 8.92893 30.9289C7.05357 32.8043 6 35.3478 6 38V40C6 42.2 7.8 44 10 44H38C39.0609 44 40.0783 43.5786 40.8284 42.8284C41.5786 42.0783 42 41.0609 42 40Z"
       fill={mainFill || fill}
     />
-  </motion.svg>
+  </svg>
 );
